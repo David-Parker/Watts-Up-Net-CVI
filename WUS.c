6c02eb8d583b7fc04925906ad8040e44a89694fc
David Parker <david.parker@ni.com>
2014-07-14 16:24:14 -0500

    Initial Commit

---
+++ WUS.c
@@ -0,0 +1,406 @@
+/***************************************************************************************
+ * (c) Copyright 2014, National Instruments. All Rights Reserved.                      *
+ ***************************************************************************************/
+/***************************************************************************************
+ * Watts Up Series                          
+ * VXIPNP, LabWindows/CVI 2012 Instrument Driver
+ *
+ *  Original Release: TODO 
+ *  By: David Parker
+ *
+ *  Modification History: 
+ *
+ *      TODO  - Instrument Driver Created.
+ ***************************************************************************************/
+#include <formatio.h>
+#include <visa.h>
+#include <ansi_c.h>
+#include "WUS.h"
+
+/*= DEFINES ===========================================================================*/
+#define WUS_REVISION			 "Rev 1.0, 07/2014, CVI 2012" /*  Instrument driver revision */
+#define WUS_BUFFER_SIZE		     512L         			  	  /*  File I/O buffer size 	     */
+#define WUS_BAUD_RATE			 9600                         /*  Baud Rate                  */
+#define WUS_BUFFER_SIZE_LARGE    1024L                        /*  Large buffer size          */
+#define WUS_TMO_VALUE            10000                        /*  Timeout Value              */
+
+/*= MACROS ============================================================================*/
+		
+/*- Error Checking ------------------------------------------------------------*/
+#ifndef CheckErr
+#define CheckErr(fCall) \
+        if (status = (fCall), (status = (status < 0) ? status : VI_SUCCESS)) \
+        { \
+            goto Error; \
+        } \
+        else
+#endif
+
+/*- Invalid Param Reporting ---------------------------------------------------*/
+#ifndef CheckParam
+#define CheckParam(fCall, errorCode) \
+        if (fCall) \
+        { \
+            status = (errorCode); \
+            goto Error; \
+        } \
+        else
+#endif
+
+
+/***************************************************************************************/
+/*= INSTRUMENT-DEPENDENT STATUS/RANGE STRUCTURE  ======================================*/
+/***************************************************************************************/
+/* WUS_stringValPair is used in the WUS_errorMessage function				   */
+/*=====================================================================================*/
+typedef struct  WUS_stringValPair
+{
+   ViStatus stringVal;
+   ViString stringName;
+}  WUS_tStringValPair;
+
+
+/***************************************************************************************/
+/*= INSTRUMENT SPECIFIC UTILITY ROUTINE DECLARATIONS (Non-Exportable Functions) =======*/
+/***************************************************************************************/
+ViStatus WUS_DefaultInstrSetup (ViSession vi);
+ViStatus WUS_CheckStatus (ViSession vi);
+static ViBoolean WUS_InvalidViBooleanRange (ViBoolean val);
+static ViBoolean WUS_InvalidViInt32Range (ViInt32 val, ViInt32 min, ViInt32 max);
+static ViBoolean WUS_InvalidViReal64Range (ViReal64 val, ViReal64 min, ViReal64 max);
+static ViBoolean WUS_InvalidPtr (void* value);
+
+
+
+/***************************************************************************************/
+/*====== USER-CALLABLE FUNCTIONS (Exportable Functions) ===============================*/
+/***************************************************************************************/
+
+/***************************************************************************************
+ *Function: WUS_Initialize
+ *Purpose:  Establishes communication with the instrument and optionally
+            performs an instrument identification query and/or an
+            instrument reset.  It also places the instrument in a default
+            state needed for other instrument driver operations. 
+            Therefore, call this function before calling other
+            instrument driver functions for this instrument.  Generally, you need to 
+            call the Initialize function only once at the beginning of an application.
+ ***************************************************************************************/
+ViStatus _VI_FUNC  WUS_Initialize (ViRsrc VISAResourceName, 
+        							   ViBoolean IDQuery, 
+        							   ViBoolean reset, 
+        							   ViSession* vi)
+{
+    /*Define local variables.*/
+	ViStatus status = VI_SUCCESS;
+	ViSession rmSession = 0;
+	ViUInt16  interface = VI_INTF_ASRL;
+	ViChar rdBuffer[WUS_BUFFER_SIZE];
+
+	/*- Check input parameter ranges ----------------------------------------*/
+	CheckParam (WUS_InvalidViBooleanRange(IDQuery),VI_ERROR_PARAMETER2);
+	CheckParam (WUS_InvalidViBooleanRange(reset),VI_ERROR_PARAMETER3);
+	CheckParam (WUS_InvalidPtr(vi),VI_ERROR_PARAMETER4);
+
+	/*- Open instrument session ---------------------------------------------*/
+    CheckErr (viOpenDefaultRM(&rmSession));
+    if ((status = viOpen(rmSession, VISAResourceName, VI_NULL, VI_NULL, vi)) < 0) {
+        viClose (rmSession);
+        return status;
+	}	
+	
+	/*- Configure VISA Formatted I/O ----------------------------------------*/
+    CheckErr (viSetAttribute(*vi, VI_ATTR_TMO_VALUE,WUS_TMO_VALUE));
+    CheckErr (viSetBuf(*vi, VI_READ_BUF|VI_WRITE_BUF,WUS_BUFFER_SIZE_LARGE));
+    CheckErr (viSetAttribute(*vi, VI_ATTR_WR_BUF_OPER_MODE,VI_FLUSH_ON_ACCESS));
+    CheckErr (viSetAttribute(*vi, VI_ATTR_RD_BUF_OPER_MODE,VI_FLUSH_ON_ACCESS));
+	
+	/*- Configure VISA Serial Interface -----------------------------------------------*/
+	CheckErr (viGetAttribute(*vi, VI_ATTR_INTF_TYPE, &interface));
+    
+    if (interface == VI_INTF_ASRL)
+    {
+		CheckErr (viSetAttribute(*vi, VI_ATTR_TERMCHAR_EN, VI_TRUE));
+		CheckErr (viSetAttribute(*vi, VI_ATTR_SEND_END_EN, VI_TRUE));
+		CheckErr (viSetAttribute(*vi, VI_ATTR_TERMCHAR,0xA));
+		CheckErr (viSetAttribute(*vi, VI_ATTR_ASRL_END_OUT,VI_ASRL_END_TERMCHAR));
+		CheckErr (viSetAttribute(*vi, VI_ATTR_ASRL_END_IN,VI_ASRL_END_TERMCHAR));
+		CheckErr (viSetAttribute(*vi, VI_ATTR_ASRL_BAUD, WUS_BAUD_RATE));
+		CheckErr (viSetAttribute(*vi, VI_ATTR_ASRL_FLOW_CNTRL, VI_ASRL_FLOW_RTS_CTS));
+		CheckErr (viSetAttribute(*vi, VI_ATTR_ASRL_PARITY, VI_ASRL_PAR_NONE));   
+        CheckErr (viSetAttribute(*vi, VI_ATTR_ASRL_DATA_BITS,8));
+        CheckErr (viSetAttribute(*vi, VI_ATTR_ASRL_STOP_BITS,VI_ASRL_STOP_ONE));
+    }
+	
+	/*- Identification Query ------------------------------------------------*/
+    if (IDQuery) {
+    	ViChar *queryResponse = "Stanford Research Systems,CG635";
+    	CheckErr (viQueryf(*vi,"*IDN?\n","%256[^\r]",rdBuffer));
+        if ((!strstr(rdBuffer,queryResponse)))
+            CheckErr (VI_ERROR_FAIL_ID_QUERY);
+    }
+
+    /*- Reset instrument ----------------------------------------------------*/
+    if (reset)
+        CheckErr (WUS_Reset(*vi));
+    else  /* - Send Default Instrument Setup --------------------------------- */
+        CheckErr (WUS_DefaultInstrSetup(*vi));
+Error:
+	return status;
+}
+
+/***************************************************************************************
+ *Function: WUS_Close
+ *Purpose:  Takes the instrument offline.
+ ***************************************************************************************/
+ViStatus _VI_FUNC  WUS_Close (ViSession vi)
+{
+	/*Define local variables.*/
+	ViSession rmSession;
+	ViStatus status = VI_SUCCESS;	
+
+	/*- Check input parameter ranges ----------------------------------------*/
+	CheckErr (viGetAttribute(vi, VI_ATTR_RM_SESSION, &rmSession));
+	
+	status = viClose (vi);
+	viClose (rmSession);
+
+Error:
+    return status;
+}
+
+
+/***************************************************************************************
+ *Function: WUS_Wait
+ *Purpose:  Waits until all previously queued commands have completed finishing
+            before proceeding.
+ ***************************************************************************************/
+ViStatus _VI_FUNC  WUS_Wait (ViSession vi)
+{
+    /*Define local variables.*/
+	ViStatus status = VI_SUCCESS;
+
+	CheckErr (viPrintf(vi,"*WAI;"));
+	CheckErr (WUS_CheckStatus(vi));
+
+Error:
+	return status;
+}
+
+
+/***************************************************************************************
+ * Function: Error Message                                                   
+ * Purpose:  This function translates the error return value from the        
+             instrument driver into a user-readable string.                  
+ **************************************************************************************/
+ViStatus _VI_FUNC WUS_ErrorMessage (ViSession vi, 
+										ViInt32 statusCode,
+        								ViChar errorMessage[])
+{
+    /*Define local variables.*/
+    ViStatus status = VI_SUCCESS;
+    ViInt32 i;
+
+    static WUS_tStringValPair statusDescArray[] = {
+    {VI_ERROR_PARAMETER1,                   "ERROR: Parameter 1 out of range" },
+    {VI_ERROR_PARAMETER2,                   "ERROR: Parameter 2 out of range" },
+    {VI_ERROR_PARAMETER3,                   "ERROR: Parameter 3 out of range" },
+    {VI_ERROR_PARAMETER4,                   "ERROR: Parameter 4 out of range" },
+    {VI_ERROR_PARAMETER5,                   "ERROR: Parameter 5 out of range" },
+    {VI_NULL,VI_NULL}
+    };
+
+	/*- Check input parameter ranges ----------------------------------------*/
+	CheckParam (WUS_InvalidPtr(errorMessage),VI_ERROR_PARAMETER3);
+    
+    status = viStatusDesc (vi,statusCode,errorMessage);
+    if (status == VI_WARN_UNKNOWN_STATUS){
+   		for (i = 0; statusDescArray[i].stringName; i++){
+   			if (statusDescArray[i].stringVal==statusCode){
+   				sprintf(errorMessage,"%s",statusDescArray[i].stringName);
+   				return VI_SUCCESS;
+   			}
+   		}
+   		sprintf (errorMessage,"Unknown Error 0x%x", statusCode);
+   		return VI_WARN_UNKNOWN_STATUS;
+   }
+
+Error:
+   status = VI_SUCCESS;
+   return status;
+}
+
+/***************************************************************************************
+ *Function: WUS_Reset
+ *Purpose:  Resets the instrument and then sends a set of default setup
+            commands to the instrument.
+ ***************************************************************************************/
+ViStatus _VI_FUNC  WUS_Reset (ViSession vi)
+{
+    /*Define local variables.*/
+    ViStatus status = VI_SUCCESS;
+
+    /*  Initialize the instrument to a known state.  */
+    CheckErr (viPrintf(vi, "*RST"));
+    CheckErr (WUS_DefaultInstrSetup(vi));
+    CheckErr (WUS_CheckStatus(vi));
+Error:
+    return status;
+}
+
+/***************************************************************************************
+ *Function: WUS_RevisionQuery
+ *Purpose:  Queries the current instrument firmware revision and instrument
+            driver revision. 
+            Refer to the Readme file for detailed driver information including 
+            modification history.
+ ***************************************************************************************/
+ViStatus _VI_FUNC  WUS_RevisionQuery (ViSession vi, 
+        								  ViChar instrumentDriverRevision[], 
+        								  ViChar instrumentFirmwareRevision[])
+{
+    /*Define local variables.*/
+    ViStatus status = VI_SUCCESS;
+    ViChar rdBuf[WUS_BUFFER_SIZE];
+    
+    /*- Check input parameter ranges ----------------------------------------*/
+    CheckParam (WUS_InvalidPtr(instrumentDriverRevision),VI_ERROR_PARAMETER2);
+    CheckParam (WUS_InvalidPtr(instrumentFirmwareRevision),VI_ERROR_PARAMETER3);
+
+    CheckErr (viQueryf(vi,"*IDN?\n","%256[^\r]",rdBuf));
+    strcpy (instrumentFirmwareRevision,strstr(rdBuf,"ver"));
+    instrumentFirmwareRevision[strlen(instrumentFirmwareRevision)-1] = '\0';
+    strcpy (instrumentDriverRevision,WUS_REVISION);
+    CheckErr (WUS_CheckStatus(vi));
+
+Error:
+    return status;
+}
+
+
+/***************************************************************************************
+ *Function: WUS_SelfTest
+ *Purpose:  Runs the instrument's self-test routine and returns the test
+            results.
+ ***************************************************************************************/
+ViStatus _VI_FUNC  WUS_SelfTest (ViSession vi, 
+        							 ViInt32* selfTestResultCode, 
+        							 ViChar selfTestResultMessage[])
+{
+    /*Define local variables.*/
+    ViStatus status = VI_SUCCESS;
+    ViByte rdBuf[WUS_BUFFER_SIZE];
+
+    /*- Check input parameter ranges ----------------------------------------*/
+    CheckParam (WUS_InvalidPtr(selfTestResultCode),VI_ERROR_PARAMETER2);
+    CheckParam (WUS_InvalidPtr(selfTestResultMessage),VI_ERROR_PARAMETER3);
+
+    CheckErr (viQueryf(vi,"*TST?","%s",rdBuf));
+    Scan (rdBuf,"%s>%d",selfTestResultCode);
+    Scan (rdBuf,"%s>%s",selfTestResultMessage);
+    CheckErr (WUS_CheckStatus(vi));
+Error:
+    return status;		
+}
+
+/*************************************************************************************************/
+/* Function: Boolean Value Out Of Range - ViBoolean                                              */
+/* Purpose:  This function checks a Boolean to see if it is equal to VI_TRUE or VI_FALSE.        */
+/*           If the value is out of range, the return value is VI_TRUE, otherwise the return     */
+/*           the return value is VI_FALSE.                                                       */
+/*************************************************************************************************/
+static ViBoolean WUS_InvalidViBooleanRange (ViBoolean val)
+{
+    return ((val != VI_FALSE && val != VI_TRUE) ? VI_TRUE : VI_FALSE);
+}
+
+/*************************************************************************************************/
+/* Function: Long Signed Integer Value Out Of Range - ViInt32                                    */
+/* Purpose:  This function checks a long signed integer value to see if it lies between a        */
+/*           minimum and maximum value.  If the value is out of range, the return value is       */
+/*           VI_TRUE, otherwise the return value is VI_FALSE.                                    */
+/*************************************************************************************************/
+static ViBoolean WUS_InvalidViInt32Range (ViInt32 val, ViInt32 min, ViInt32 max)
+{
+    return ((val < min || val > max) ? VI_TRUE : VI_FALSE);
+}
+
+/*************************************************************************************************/
+/* Function: Real (Double) Value Out Of Range - ViReal64                                         */
+/* Purpose:  This function checks a real (double) value to see if it lies between a minimum      */
+/*           and maximum value.  If the value is out of range, the return value is VI_TRUE,      */
+/*           otherwise the return value is VI_FALSE.                                             */
+/*************************************************************************************************/
+static ViBoolean WUS_InvalidViReal64Range (ViReal64 val, ViReal64 min, ViReal64 max)
+{
+    return ((val < min || val > max) ? VI_TRUE : VI_FALSE);
+}
+
+/*************************************************************************************************/
+/* Function: Pointer value is VI_NULL - void *                                                   */
+/* Purpose:  This function checks an output parameter (pointer) to see if its value is VI_NULL.  */
+/*           If the value is VI_NULL, the return value is VI_TRUE, otherwise the return value    */
+/*           is VI_FALSE.                                                                        */
+/*************************************************************************************************/
+static ViBoolean WUS_InvalidPtr (void *value)
+{
+    return ((value == VI_NULL) ? VI_TRUE : VI_FALSE);
+}
+
+
+/*****************************************************************************/
+/*----------- INSERT INSTRUMENT-DEPENDENT UTILITY ROUTINES HERE -------------*/
+/*****************************************************************************/
+
+/*****************************************************************************************************/
+/* Function: Default Instrument Setup                                                                */
+/* Purpose:  This function sends a default setup to the instrument.  This function is called by      */
+/*           the stsg380_reset operation and by the stsg380_init function if the reset option has    */
+/*           not been selected.  This function is useful for configuring any  instrument settings    */
+/*           that are required by the rest of the  instrument driver functions such as turning       */
+/*           headers ON or OFF or using the long or short form for commands, queries, and data.      */
+/*****************************************************************************************************/
+ViStatus WUS_DefaultInstrSetup (ViSession instrSession)
+{
+    ViStatus status = VI_SUCCESS;
+
+    return status;
+}
+
+/***************************************************************************************
+ * Function: WUS_CheckStatus
+ *
+ * Purpose:  This function check if there is command error or command execution error by
+ *           check the Event Status Register of the instrument.
+ ***************************************************************************************/
+ViStatus WUS_CheckStatus (ViSession vi)
+{
+    ViStatus    status = VI_SUCCESS;
+    ViUInt32    esrValue = 0;
+
+    CheckErr (viQueryf(vi, "*ESR?\n", "%d", &esrValue));
+
+    /*---------------- Check if any error or message bit was asserted -------*/
+    if ((esrValue & WUS_ESR_QUERY_ERROR) != 0)
+    {
+        CheckErr (WUS_ESR_QUERY_ERROR);
+    }
+    if ((esrValue & WUS_ESR_DEVICE_DEPENDENT_ERROR) != 0)
+    {
+        CheckErr (WUS_ERROR_DEVICE_DEPENDENT_ERROR);
+    }
+    if ((esrValue & WUS_ESR_EXECUTION_ERROR) != 0)
+    {
+        CheckErr (WUS_ERROR_EXECUTION_ERROR);
+    } 
+	if ((esrValue & WUS_ESR_COMMAND_ERROR) != 0)
+    {
+        CheckErr (WUS_ERROR_COMMAND_ERROR);
+    }
+
+Error:
+    return status;
+}
+/*****************************************************************************
+ *-------------------------- End Instrument Driver Source Code --------------*
+ *****************************************************************************/
---
+++ WUS.h
@@ -0,0 +1,113 @@
+/***************************************************************************************
+ *
+ * (c) Copyright 2014, National Instruments. All Rights Reserved.
+ *
+ * Title:    WUS.h
+ *
+ * Purpose:  
+ *           VXIPNP instrument driver declarations.
+ *
+/***************************************************************************************/
+
+
+#ifndef _WUS_HEADER_
+#define _WUS_HEADER_
+#include <vpptype.h>
+
+#if defined(__cplusplus) || defined(__cplusplus__)
+extern "C" {
+#endif
+
+
+/***************************************************************************************/
+/*= Instrument Driver Specific Error/Warning Codes ====================================*/
+/***************************************************************************************/
+#define VI_ERROR_INSTR_FILE_OPEN                	           (_VI_ERROR+0x3FFC0800L)
+#define VI_ERROR_INSTR_FILE_WRITE               	           (_VI_ERROR+0x3FFC0801L)
+#define VI_ERROR_INSTR_INTERPRETING_RESPONSE    	           (_VI_ERROR+0x3FFC0803L)
+#define VI_ERROR_INSTR_PARAMETER9               	           (_VI_ERROR+0x3FFC0809L)
+#define VI_ERROR_INSTR_PARAMETER10              	           (_VI_ERROR+0x3FFC080AL)
+#define VI_ERROR_INSTR_PARAMETER11              	           (_VI_ERROR+0x3FFC080BL)
+#define VI_ERROR_INSTR_PARAMETER12              	           (_VI_ERROR+0x3FFC080CL)
+#define VI_ERROR_INSTR_PARAMETER13              	           (_VI_ERROR+0x3FFC080DL)
+#define VI_ERROR_INSTR_PARAMETER14              	           (_VI_ERROR+0x3FFC080EL)
+#define VI_ERROR_INSTR_PARAMETER15              	           (_VI_ERROR+0x3FFC080FL)
+
+/***************************************************************************************/
+/*= Instrument Specific Error/Warning Codes ===========================================*/
+/***************************************************************************************/
+#define VI_WARNING_INSTR_OFFSET						           (0x3FFC0900L)
+#define VI_ERROR_INSTR_OFFSET						           (_VI_ERROR+0x3FFC0900L)
+#define WUS_ERROR_DEVICE_DEPENDENT_ERROR                   (VI_ERROR_INSTR_OFFSET + 0x10L)
+#define WUS_ERROR_EXECUTION_ERROR                          (VI_ERROR_INSTR_OFFSET + 0x20L)
+#define WUS_ERROR_COMMAND_ERROR                            (VI_ERROR_INSTR_OFFSET + 0x30L)
+
+/***************************************************************************************/
+/*= INSTRUMENT CONSTANTS ==============================================================*/
+/***************************************************************************************/
+
+/* Defined values for Standard Event Status Register */
+#define WUS_ESR_QUERY_ERROR                                 0x04
+#define WUS_ESR_DEVICE_DEPENDENT_ERROR                      0x08
+#define WUS_ESR_EXECUTION_ERROR                             0x10
+#define WUS_ESR_COMMAND_ERROR                               0x20
+
+
+/***************************************************************************************/
+/*= GLOBAL USER-CALLABLE FUNCTION DECLARATIONS (Exportable Functions) =================*/
+/***************************************************************************************/
+
+ViStatus _VI_FUNC  WUS_Initialize (ViRsrc VISAResourceName, 
+        							   ViBoolean IDQuery, 
+        							   ViBoolean reset, 
+        						       ViSession* vi);
+
+ViStatus _VI_FUNC  WUS_Close (ViSession vi);
+
+/*-------------------------------------------------------------------------------------*/
+/*--------------------------------- Action-Status--------------------------------------*/
+/*-------------------------------------------------------------------------------------*/
+
+ViStatus _VI_FUNC  WUS_Wait (ViSession vi);
+
+/*-------------------------------------------------------------------------------------*/
+/*--------------------------------- Configure------------------------------------------*/
+/*-------------------------------------------------------------------------------------*/
+
+
+/*-------------------------------------------------------------------------------------*/
+/*--------------------------------- Low-level------------------------------------------*/
+/*-------------------------------------------------------------------------------------*/
+
+/*-------------------------------------------------------------------------------------*/
+/*--------------------------------- Utility--------------------------------------------*/
+/*-------------------------------------------------------------------------------------*/
+ViStatus _VI_FUNC  WUS_ErrorQuery (ViSession vi, 
+        							   ViInt32* errorCode, 
+        							   ViChar errorMessage[]);
+
+ViStatus _VI_FUNC WUS_ErrorMessage (ViSession vi, 
+        								ViInt32 statusCode,
+        								ViChar errorMessage[]);
+
+ViStatus _VI_FUNC  WUS_Reset (ViSession vi);
+
+ViStatus _VI_FUNC  WUS_RevisionQuery (ViSession vi, 
+        								  ViChar instrumentDriverRevision[], 
+        								  ViChar instrumentFirmwareRevision[]);
+
+ViStatus _VI_FUNC  WUS_SelfTest (ViSession vi, 
+        							 ViInt32* selfTestResultCode, 
+        							 ViChar selfTestResultMessage[]);
+
+
+
+#if defined(__cplusplus) || defined(__cplusplus__)
+}
+#endif
+#endif
+
+
+/***************************************************************************************/
+/*=== END INCLUDE FILE ================================================================*/
+/***************************************************************************************/
